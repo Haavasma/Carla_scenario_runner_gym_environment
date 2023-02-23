5bfc54c4ff9c533a019b34f92ad0f86d1e08f7a3
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-02-23 06:38:19 +0100

    updated packaging

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup(
-    packages=[
-        package for package in find_packages() if package.startswith("episode_manager")
-    ],
-    name="gym_carla",
+    packages=[package for package in find_packages() if package.startswith("gym_env")],
+    name="srunner_gym_env",
     version="0.0.1",
     install_requires=[
         "episode_manager @ git+https://github.com/Haavasma/episode_manager.git",
