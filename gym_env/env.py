1b26f1ed8b30586abb2dfb08424ba73f514f3163
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-03-01 19:10:11 +0100

    initial testing with transfuser integration

--- gym_env/env.py
+++ gym_env/env.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass, field
-from typing import Any, Callable, List, Protocol, Tuple
+from typing import Callable, List, Protocol, Tuple
 from episode_manager.episode_manager import Action, WorldState
 import gym
 from gym.spaces import Box, Dict, Discrete
 from gym.utils import seeding
-from episode_manager import EpisodeManager
 import numpy as np
+import pygame
+
+
+from episode_manager import EpisodeManager
 
 
 class VisionModule(Protocol):
@@ -14,7 +17,7 @@ class VisionModule(Protocol):
     the environment to provide vision encoding and selected action postprocessing
     """
 
-    output_shape: Tuple[Any]
+    output_shape: Tuple
     high: float
     low: float
 
@@ -33,6 +36,12 @@ class VisionModule(Protocol):
         # return the same action by default
 
         return action
+
+    def get_auxilliary_render(self) -> pygame.Surface:
+        """
+        Returns a pygame surface that visualizes auxilliary predections from the vision module
+        """
+        raise NotImplementedError
 
 
 @dataclass
@@ -63,15 +72,24 @@ class PIDController:
 
 
 @dataclass
-class CarlaEnvVisionModule(gym.Env):
+class SteeringController:
+    kp: float = 0.0
+    ki: float = 0.0
+    kd: float = 0.0
+
+    def __call__(self, goal_angle: float, current_angle: float) -> float:
+        return 0
+
+
+@dataclass
+class CarlaEnvironment(gym.Env):
     config: CarlaEnvironmentConfiguration
     carla_manager: EpisodeManager
     vision_module: VisionModule
-    reward_function: Callable[[WorldState], float]
-    is_done: Callable[[WorldState], bool]
-    pid_controller: PIDController
+    reward_function: Callable[[WorldState], Tuple[float, bool]]
+    speed_controller: PIDController
 
-    def __init__(self):
+    def __post_init__(self):
         """
         Sets up action and observation space based on configurations
         """
@@ -99,6 +117,55 @@ class CarlaEnvVisionModule(gym.Env):
                 ),
             )
 
+        observation_space_dict = (
+            self._set_observation_space_without_vision()
+            if self.vision_module is None
+            else self._set_observation_space_with_vision()
+        )
+
+        self.observation_space = Dict(spaces=observation_space_dict)
+
+        return
+
+    def _set_observation_space_without_vision(self) -> dict:
+        observation_space_dict = {}
+        lidar = self.carla_manager.config.car_config.lidar
+
+        if self.carla_manager.config.car_config.lidar.enabled:
+            observation_space_dict["lidar"] = Box(
+                low=0,
+                high=255,
+                shape=lidar.shape,
+                dtype=np.uint8,
+            )
+
+        camera_configs = self.carla_manager.config.car_config.cameras
+
+        for index, camera in enumerate(camera_configs):
+            observation_space_dict[f"image_index{index}"] = Box(
+                low=0,
+                high=255,
+                shape=(camera.height, camera.width, 4),
+                dtype=np.uint8,
+            )
+
+        observation_space_dict["state"] = Box(
+            low=min(
+                self.config.continuous_speed_range
+                if self.config.discrete_actions
+                else self.config.speed_goal_actions
+            ),
+            high=max(
+                self.config.continuous_speed_range
+                if self.config.discrete_actions
+                else self.config.speed_goal_actions
+            ),
+            dtype=np.float32,
+        )
+
+        return observation_space_dict
+
+    def _set_observation_space_with_vision(self) -> dict:
         observation_space_dict = {
             "vision_encoding": Box(
                 low=self.vision_module.low,
@@ -106,31 +173,64 @@ class CarlaEnvVisionModule(gym.Env):
                 shape=self.vision_module.output_shape,
                 dtype=np.float32,
             ),
-            "state": Box(
-                low=min(
-                    self.config.continuous_speed_range
-                    if self.config.discrete_actions
-                    else self.config.speed_goal_actions
-                ),
-                high=max(
-                    self.config.continuous_speed_range
-                    if self.config.discrete_actions
-                    else self.config.speed_goal_actions
-                ),
-                dtype=np.float32,
-            ),
         }
-        self.observation_space = Dict(spaces=observation_space_dict)
 
-        return
+        observation_space_dict["state"] = Box(
+            min(
+                np.array(
+                    [
+                        self.config.continuous_speed_range
+                        if self.config.discrete_actions
+                        else self.config.speed_goal_actions
+                    ]
+                )
+            ),
+            max(
+                np.array(
+                    [
+                        self.config.continuous_speed_range
+                        if self.config.discrete_actions
+                        else self.config.speed_goal_actions
+                    ]
+                )
+            ),
+            dtype=np.float32,
+        )
+
+        return observation_space_dict
 
     def reset(self):
         self.carla_manager.stop_episode()
         self.state = self.carla_manager.start_episode()
+        print("RESET EPISODE")
 
         return self._get_obs()
 
     def _get_obs(self):
+        return (
+            self._get_obs_without_vision()
+            if self.vision_module is None
+            else self._get_obs_with_vision()
+        )
+
+    def _get_obs_without_vision(self):
+        observation = {}
+
+        for index, image in enumerate(self.state.ego_vehicle_state.sensor_data.images):
+            observation[f"image_{index}"] = image
+
+        if self.state.ego_vehicle_state.sensor_data.lidar_data:
+            observation[
+                "lidar"
+            ] = self.state.ego_vehicle_state.sensor_data.lidar_data.bev
+
+        observation = {
+            "state": self.state.ego_vehicle_state.speed,
+        }
+
+        return observation
+
+    def _get_obs_with_vision(self):
         vision_encoding = self.vision_module(self.state)
         # TODO: get direction of target point, and next high level command, and use as observation state for the RL model
 
@@ -141,7 +241,7 @@ class CarlaEnvVisionModule(gym.Env):
 
         return observation
 
-    def step(self, action):
+    def step(self, action: Tuple[float, float]):
         goal_speed = 0.0
         steering = 0.0
 
@@ -167,16 +267,15 @@ class CarlaEnvVisionModule(gym.Env):
             goal_speed, self.state.ego_vehicle_state.speed
         )
 
-        action = Action(throttle, brake, reverse, steering)
-        action = self.vision_module.postprocess_action(action)
+        new_action = Action(throttle, brake, reverse, steering)
+        new_action = self.vision_module.postprocess_action(new_action)
 
         # update state with result of using the new action
-        self.state = self.carla_manager.step(Action(throttle, brake, reverse, steering))
+        self.state = self.carla_manager.step(new_action)
 
-        reward = self.reward_function(self.state)
-        done = self.is_done(self.state)
+        reward, done = self.reward_function(self.state)
 
-        return (self._get_obs(), reward, done)
+        return (self._get_obs_with_vision(), reward, done)
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    packages=[package for package in find_packages() if package.startswith("gym_env")],
-    name="srunner_gym_env",
+    packages=["/"],
+    name="carla_gym_env",
     version="0.0.1",
     install_requires=[
-        "episode_manager @ git+https://github.com/Haavasma/episode_manager.git",
+        # "episode_manager @ git+https://github.com/Haavasma/episode_manager.git",
         "gym",
     ],
 )
