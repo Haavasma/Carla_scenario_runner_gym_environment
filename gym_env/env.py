6af5ad600b857757a337971957de62f49ff03dbb
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-03-07 00:05:14 +0100

    fixed environment runtime errors

--- gym_env/env.py
+++ gym_env/env.py
@@ -1,14 +1,14 @@
-from dataclasses import dataclass, field
-from typing import Callable, List, Protocol, Tuple
-from episode_manager.episode_manager import Action, WorldState
+from dataclasses import dataclass
+from time import time
+from typing import Callable, List, Protocol, Tuple, TypedDict
+
 import gym
-from gym.spaces import Box, Dict, Discrete
-from gym.utils import seeding
 import numpy as np
 import pygame
-
-
 from episode_manager import EpisodeManager
+from episode_manager.episode_manager import Action, WorldState
+from gym.spaces import Box, Dict, Discrete
+from gym.utils import seeding
 
 
 class VisionModule(Protocol):
@@ -44,14 +44,22 @@ class VisionModule(Protocol):
         raise NotImplementedError
 
 
-@dataclass
-class CarlaEnvironmentConfiguration:
-    continuous_speed_range: Tuple[float, float] = (-2.0, 10.0)  # m/s
-    continuous_steering_range: Tuple[float, float] = (-0.3, 0.3)  # rad i think
+class CarlaEnvironmentConfiguration(TypedDict):
+    continuous_speed_range: Tuple[float, float]
+    continuous_steering_range: Tuple[float, float]
+    speed_goal_actions: List[float]
+    steering_actions: List[float]
+    discrete_actions: bool
 
-    speed_goal_actions: List[float] = field(default_factory=lambda: [])
-    steering_actions: List[float] = field(default_factory=lambda: [])
-    discrete_actions: bool = True
+
+def default_config() -> CarlaEnvironmentConfiguration:
+    return {
+        "continuous_speed_range": (-2.0, 10.0),
+        "continuous_steering_range": (-0.3, 0.3),
+        "speed_goal_actions": [],
+        "steering_actions": [],
+        "discrete_actions": True,
+    }
 
 
 @dataclass
@@ -93,10 +101,10 @@ class CarlaEnvironment(gym.Env):
         """
         Sets up action and observation space based on configurations
         """
-        self.amount_of_speed_actions = len(self.config.speed_goal_actions)
-        self.amount_of_steering_actions = len(self.config.steering_actions)
+        self.amount_of_speed_actions = len(self.config["speed_goal_actions"])
+        self.amount_of_steering_actions = len(self.config["steering_actions"])
 
-        if self.config.discrete_actions:
+        if self.config["discrete_actions"]:
             self.action_space = Discrete(
                 self.amount_of_steering_actions * self.amount_of_speed_actions
             )
@@ -105,14 +113,14 @@ class CarlaEnvironment(gym.Env):
             self.action_space = Box(
                 np.array(
                     [
-                        self.config.continuous_speed_range[0],
-                        self.config.continuous_steering_range[0],
+                        self.config["continuous_speed_range"][0],
+                        self.config["continuous_steering_range"][0],
                     ]
                 ),
                 np.array(
                     [
-                        self.config.continuous_speed_range[1],
-                        self.config.continuous_steering_range[1],
+                        self.config["continuous_speed_range"][1],
+                        self.config["continuous_steering_range"][1],
                     ]
                 ),
             )
@@ -131,35 +139,35 @@ class CarlaEnvironment(gym.Env):
         observation_space_dict = {}
         lidar = self.carla_manager.config.car_config.lidar
 
-        if self.carla_manager.config.car_config.lidar.enabled:
+        print("lidar SHAPE", lidar["shape"])
+
+        if self.carla_manager.config.car_config.lidar["enabled"]:
             observation_space_dict["lidar"] = Box(
                 low=0,
                 high=255,
-                shape=lidar.shape,
+                shape=lidar["shape"],
                 dtype=np.uint8,
             )
 
         camera_configs = self.carla_manager.config.car_config.cameras
 
         for index, camera in enumerate(camera_configs):
-            observation_space_dict[f"image_index{index}"] = Box(
+            observation_space_dict[f"image_{index}"] = Box(
                 low=0,
                 high=255,
-                shape=(camera.height, camera.width, 4),
+                shape=(camera["height"], camera["width"], 3),
                 dtype=np.uint8,
             )
 
+        speed_range = (
+            self.config["speed_goal_actions"]
+            if self.config["discrete_actions"]
+            else self.config["continuous_speed_range"]
+        )
+
         observation_space_dict["state"] = Box(
-            low=min(
-                self.config.continuous_speed_range
-                if self.config.discrete_actions
-                else self.config.speed_goal_actions
-            ),
-            high=max(
-                self.config.continuous_speed_range
-                if self.config.discrete_actions
-                else self.config.speed_goal_actions
-            ),
+            low=np.array([min(speed_range)]),
+            high=np.array([max(speed_range)]),
             dtype=np.float32,
         )
 
@@ -179,18 +187,18 @@ class CarlaEnvironment(gym.Env):
             min(
                 np.array(
                     [
-                        self.config.continuous_speed_range
-                        if self.config.discrete_actions
-                        else self.config.speed_goal_actions
+                        self.config["continuous_speed_range"]
+                        if self.config["discrete_actions"]
+                        else self.config["speed_goal_actions"]
                     ]
                 )
             ),
             max(
                 np.array(
                     [
-                        self.config.continuous_speed_range
-                        if self.config.discrete_actions
-                        else self.config.speed_goal_actions
+                        self.config["continuous_speed_range"]
+                        if self.config["discrete_actions"]
+                        else self.config["speed_goal_actions"]
                     ]
                 )
             ),
@@ -217,16 +225,14 @@ class CarlaEnvironment(gym.Env):
         observation = {}
 
         for index, image in enumerate(self.state.ego_vehicle_state.sensor_data.images):
-            observation[f"image_{index}"] = image
+            observation[f"image_{index}"] = image[:, :, :3]
 
         if self.state.ego_vehicle_state.sensor_data.lidar_data:
             observation[
                 "lidar"
             ] = self.state.ego_vehicle_state.sensor_data.lidar_data.bev
 
-        observation = {
-            "state": self.state.ego_vehicle_state.speed,
-        }
+        observation["state"] = self.state.ego_vehicle_state.speed
 
         return observation
 
@@ -241,19 +247,17 @@ class CarlaEnvironment(gym.Env):
 
         return observation
 
-    def step(self, action: Tuple[float, float]):
+    def step(self, action):
         goal_speed = 0.0
         steering = 0.0
+        start_time = time()
 
-        if self.config.discrete_actions:
-            if not isinstance(action, int):
-                raise ValueError("Action must be an integer")
-
-            goal_speed = self.config.speed_goal_actions[
-                action // self.amount_of_speed_actions
+        if self.config["discrete_actions"]:
+            goal_speed = self.config["speed_goal_actions"][
+                action // self.amount_of_steering_actions
             ]
 
-            steering = self.config.steering_actions[
+            steering = self.config["steering_actions"][
                 action % self.amount_of_steering_actions
             ]
 
@@ -263,19 +267,26 @@ class CarlaEnvironment(gym.Env):
 
             goal_speed, steering = action
 
-        throttle, brake, reverse = self.pid_controller(
+        throttle, brake, reverse = self.speed_controller(
             goal_speed, self.state.ego_vehicle_state.speed
         )
 
         new_action = Action(throttle, brake, reverse, steering)
-        new_action = self.vision_module.postprocess_action(new_action)
+
+        if self.vision_module is not None:
+            new_action = self.vision_module.postprocess_action(new_action)
 
         # update state with result of using the new action
         self.state = self.carla_manager.step(new_action)
 
         reward, done = self.reward_function(self.state)
 
-        return (self._get_obs_with_vision(), reward, done)
+        print(f"TIME TO RUN STEP: {time() - start_time} seconds")
+
+        result = (self._get_obs(), reward, done, {})
+        print(result[0]["image_0"].shape)
+
+        return (self._get_obs(), reward, done, {})
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
